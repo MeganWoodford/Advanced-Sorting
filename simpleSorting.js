87ff47ee7f196e9ebf0f869a55e0fceef9ef14fb
MeganWoodford <megan.woodford93@gmail.com>
2017-02-09 20:09:24 +0000

    Sorting exercises

---
+++ simpleSorting.js
@@ -0,0 +1,8 @@
+var array = [10, 2, 5, 1, 9];
+array.sort(function(a,b) {
+  return b - a
+});
+
+// Read the above article and write JavaScript code
+// to sort the array numerically
+// (the sorted array should equal [1, 2, 5, 9, 10])
